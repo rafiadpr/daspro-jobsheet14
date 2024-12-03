077393dbf830386509da937edda029cfc3a7d52b
rafiadpr <work.rafiadrianp@gmail.com>
2024-12-03 10:17:24 +0700

    new code on tugas

--- Tugas_23.java
+++ Tugas_23.java
@@ -1,24 +1,40 @@
 
+import java.util.Scanner;
+
 public class Tugas_23 {
 
     public static void main(String[] args) {
 
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Masukkan jumlah angka yang dihitung : ");
+        int jumlah = input.nextInt();
+
+        int[] angka = new int[jumlah];
+
+        for (int i = jumlah; i >= 1; i--) {
+            System.out.print("Masukkan angka ke-" + i + " : ");
+            angka[i - 1] = input.nextInt();
+        }
+
+        System.out.println("Total Rekursif : " + hitungRekursif(angka, jumlah));
+        System.out.println("Total Iteratif : " + hitungIteratif(angka));
+
     }
 
-    static int faktorialRekursif(int n) {
+    static int hitungRekursif(int angka[], int n) {
         if (n == 0) {
-            return (1);
+            return 0;
         } else {
-            return (n * faktorialRekursif(n - 1));
+            return angka[n - 1] + hitungRekursif(angka, n - 1);
         }
     }
 
-    static int faktorialIteratif(int n) {
-        int faktor = 1;
-        for (int i = n; i >= 1; i--) {
-            faktor = faktor * i;
+    static int hitungIteratif(int angka[]) {
+        int hasil = 0;
+        for (int i = 0; i < angka.length; i++) {
+            hasil += angka[i];
         }
-        return faktor;
+        return hasil;
     }
-
 }
